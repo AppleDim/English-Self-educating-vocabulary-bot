db8743210914bf309d7805310878077d0030e282
AppleDim <pollydimmi@mail.ru>
2023-05-24 00:27:42 +0300

    added util package for utility classes

---
+++ src/main/java/dmitry/polyakov/utils/HtmlParser.java
@@ -0,0 +1,26 @@
+package dmitry.polyakov.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.ValidationException;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+import static dmitry.polyakov.constants.BotStateEnum.DEFAULT_STATE;
+
+@Slf4j
+public class HtmlParser {
+    public Document connectToUrl(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException | ValidationException | NullPointerException e) {
+            log.warn("Error occurred while connecting to URL: " + url);
+        }
+
+        return doc;
+    }
+
+
+}
