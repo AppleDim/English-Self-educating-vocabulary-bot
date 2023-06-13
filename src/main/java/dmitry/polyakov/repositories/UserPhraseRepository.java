fdbde404bb2ebd70badb91eae99b82dd3b781004
AppleDim <pollydimmi@mail.ru>
2023-06-13 19:35:10 +0300

    refactoring changes

--- src/main/java/dmitry/polyakov/repositories/UserPhraseRepository.java
+++ src/main/java/dmitry/polyakov/repositories/UserPhraseRepository.java
@@ -1,12 +1,13 @@
 package dmitry.polyakov.repositories;
 
+import dmitry.polyakov.models.Phrase;
 import dmitry.polyakov.models.UserPhrase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
+import java.util.List;
 
 @Repository
 public interface UserPhraseRepository extends JpaRepository<UserPhrase, Long> {
@@ -15,7 +16,7 @@ public interface UserPhraseRepository extends JpaRepository<UserPhrase, Long> {
             "JOIN up.user u " +
             "JOIN up.phrase p " +
             "WHERE u.userId = :userId")
-    Set<String> findUserPhrasesByUserId(@Param("userId") Long userId);
+    List<String> findUserPhrasesByUserId(@Param("userId") Long userId);
 
     boolean existsByUserUserIdAndPhrasePhraseId(Long userId, Long phraseId);
 }
